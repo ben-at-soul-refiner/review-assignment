84a0aaac13708b08cc4f7a0974549c8305331280
Ben </dev/null>
2023-03-16 12:03:27 -0400

    add contact details, more emojis

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import { createOAuthDeviceAuth } from '@octokit/auth-oauth-device';
   echo`\n✨✨ Welcome to the Soul Refiner application process ✨✨\n`;
 
   const position = await question(
-    'Which position are you applying for today?\n1. Senior DevOps\n2. Backend Engineer\n\nEnter 1/2: ',
+    'Which position are you applying for today?\n1. Senior Infrastructure Engineer\n2. Senior Backend Engineer\n\nEnter 1/2: ',
     { choices: ['1', '2'] }
   );
 
@@ -19,20 +19,53 @@ import { createOAuthDeviceAuth } from '@octokit/auth-oauth-device';
    * @todo enter REPOSITORY here and CONTACT INFO here
    */
   if (position === '1') {
+    console.clear();
     echo`Please familiarize yourself with this repo prior to your interview\n`;
-    echo`If you do not have an interview, please contact our recuiter at \n`;
+    echo`If you do not have an interview, please contact:\n`;
+    echo`\nIn Poland:\n`;
+    echo`Alicja Raczyńska`;
+    echo`Senior IT Recruiter`;
+    echo`NTIATIVE IT Recruitment`;
+    echo`+48 694 085 275`;
+    echo`alicja@ntiative.com`;
+    echo`\nIn the United States:\n`;
+    echo`Kristina Douberly`;
+    echo`Director of Discipleship`;
+    echo`Soul Refiner`;
+    echo`561-681-9990`;
+    echo`kristina.d@soulrefiner.com`;
 
-    echo`Have a blessed day!`;
+    echo`\nHave a blessed day!`;
   } else if (position === '2') {
+    console.clear();
+    echo`If you do not have an interview, please contact:\n`;
+    echo`\n🇵🇱 In Poland:\n`;
+    echo`Alicja Raczyńska`;
+    echo`Senior IT Recruiter`;
+    echo`NTIATIVE IT Recruitment`;
+    echo`+48 694 085 275`;
+    echo`alicja@ntiative.com`;
+    echo`\n🇺🇸 In the United States:\n`;
+    echo`Kristina Douberly`;
+    echo`Director of Discipleship`;
+    echo`Soul Refiner`;
+    echo`561-681-9990`;
+    echo`kristina.d@soulrefiner.com\n\n`;
+
+    await question(
+      'If you have an interview, please press <enter> to continue\n'
+    );
+
     console.clear();
     echo`🛑🛑 Attention 🛑🛑`;
     echo`\nThis assignment tool will:\n`;
     echo` - log you into your GitHub account\n - create a repository for you (with some starter code)\n - create a pull request for you to review`;
 
-    echo`\nYour task is to do a code review on that pull request\n`;
+    echo`\n✏️  Your task is to do a code review on that pull request\n`;
 
     const proceed = await question('Do you wish to proceed?\n\nEnter y/n: ');
     if (proceed === 'n') return;
+    console.clear();
 
     const auth = createOAuthDeviceAuth({
       clientId: '797fc7c2acb5f7c1bed3',
@@ -93,6 +126,8 @@ import { createOAuthDeviceAuth } from '@octokit/auth-oauth-device';
           if (pr.status === 201) {
             echo`\n✅✅ Success! ✅✅\n`;
             echo`Please complete your code review at:\n${pr.data.html_url}`;
+
+            echo`\nHave a blessed day!`;
             break;
           } else {
             setTimeout(() => {
